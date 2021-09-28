0633a2d553373608bff8ab0f8499bd87f415e70c
Eric Energy <ericnrgnash@gmail.com>
2021-09-28 16:50:18 -0500

    renaming variable and commenting code

--- src/index.js
+++ src/index.js
@@ -3,53 +3,65 @@ import './styles/app.scss'
 import $ from 'jquery'
 
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_'
-let currentLetter = ''
-const selected = ['_']
+let clickedLetter = ''
+const selectable = ['_']
 const listItems = []
-let thisLetter = ''
-let pass = false
+let currentLetter = ''
+let rerender = false
 
 // Render content to page
 const renderList = (par1, par2) => {
+  if (rerender) {
+    $('#listOf').empty()
+    $('#alphaB').empty()
+    clickedLetter = ''
+  }
+
   // Checks which array to use
-  const check = pass ? par1 : listItems
-  $('#listOf').empty()
-  $('#alphaB').empty()
-  currentLetter = ''
+  const check = rerender ? par1 : listItems
 
   // Renders Large Letters and the titles underneath
   check.forEach((e) => {
-    thisLetter = e.title.charAt(0)
-    if (thisLetter !== currentLetter) {
-      currentLetter = thisLetter
+    // Grabs first letter of the title to chec against
+    currentLetter = e.title.charAt(0)
+    // Below creates a large letter if one does not exsist on the page
+    if (currentLetter !== clickedLetter) {
+      clickedLetter = currentLetter
       const el = $('<div>')
       el.addClass('row')
-      el.attr('id', 'row' + currentLetter)
+      el.attr('id', 'row' + clickedLetter)
       const h2 = $('<h2>')
       h2.addClass('largeLetter')
-      h2.text(currentLetter)
+      h2.text(clickedLetter)
       $('#listOf').append(h2)
       $('#listOf').append(el)
-      selected.push(currentLetter)
+      if (!selectable.includes(par2)) {
+        selectable.push(clickedLetter)
+      }
     }
+    // Adds the titles to the page
     const word = $('<a>')
     word.attr('href', e.link)
     word.attr('title', e.title)
     word.addClass('intro')
     word.text(e.title)
-    $('#row' + thisLetter).append(word)
+    $('#row' + currentLetter).append(word)
   })
 
-  // Add string of the alphabet to page with click function
+  // Adds the alphabet to page
+  // This was added at the bottom of the function
+  // This is due to parms cannot be passed with out an arrow function
+  // But I wanted to demo for loops and for each
   for (let i = 0; alphabet.length > i; i += 1) {
     const letterBtn = $('<strong>')
     letterBtn.attr('data-letter', alphabet[i])
     letterBtn.text(alphabet[i])
 
-    // This section adds styling to the clickable letters
-    if (par2 !== alphabet[i] && selected.includes(alphabet[i])) {
+    // After clicking on a letter first if statement will be hit
+    // Second else if will handle non clicked letters that are clickable
+    if (par2 === alphabet[i]) {
       letterBtn.css({ color: '#A3C8F1', cursor: 'pointer' })
-    } else if (par2 === alphabet[i]) {
+    } else if (selectable.includes(alphabet[i])) {
       letterBtn.css({
         color: '#1b76dd',
         cursor: 'pointer',
@@ -61,26 +73,32 @@ const renderList = (par1, par2) => {
   }
 }
 
-// Starts the application
+// Starts the application by calling the JSON and pushing data
+// If no data is present it will post a message
 $(() => {
-  $.ajax(
-    'https://ericenergy.github.io//HealthCare-Directory/assets/data.json'
-  ).then((res) => {
-    res.termList.forEach((element) => {
-      listItems.push(element)
+  $.ajax('https://ericenergy.github.io//HealthCare-Directory/assets/data.json')
+    .then((res) => {
+      res.termList.forEach((element) => {
+        listItems.push(element)
+      })
+      renderList()
     })
-    renderList()
-  })
+    .catch(() => {
+      const h2 = $('<h2>')
+      h2.css({ textAlign: 'center' })
+      h2.text('There is nothing to show here')
+      $('#listOf').append(h2)
+    })
 })
 
 // Handles which letter a user clicks on
 const renderClick = function () {
   const text = $(this).text()
-  if (selected.includes(text)) {
-    pass = true
+  if (selectable.includes(text)) {
+    rerender = true
     const arr =
       text === '_' ? listItems : listItems.filter((e) => e.title[0] === text)
-    const arr2 = selected.filter((e) => e.charAt(0) === text)
+    const arr2 = selectable.filter((e) => e.charAt(0) === text)
     renderList(arr, ...arr2)
   }
 }
